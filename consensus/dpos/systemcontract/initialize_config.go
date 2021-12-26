b2dcf8283029c620203148020f449030e82bcfa2
marvindx <zhoujunfeng@dxchain.onaliyun.com>
2021-12-26 18:24:14 +0800

    fix: block sync

--- consensus/dpos/systemcontract/initialize_config.go
+++ consensus/dpos/systemcontract/initialize_config.go
@@ -7,7 +7,7 @@ import (
 
 // using for Validators contract's initialize
 var (
-	InitValAddress = common.HexToAddress("0xA88548E97AF8809aFaC9dC7a930650c117951059")
+	InitValAddress = common.HexToAddress("0x6b246a131e2c59108c841e9bc725acad3d3ee3f0")
 	InitRate       = uint8(70)
 	InitDeposit    = new(big.Int).Mul(big.NewInt(1e18), big.NewInt(1))
 )
