8e86955324f51aca79ebdbb983a1ccfe152ca126
marvindx <zhoujunfeng@dxchain.onaliyun.com>
2022-01-03 22:26:02 +0800

    feat: deploy testnet v0.2.1

--- consensus/dpos/systemcontract/initialize_config.go
+++ consensus/dpos/systemcontract/initialize_config.go
@@ -7,7 +7,7 @@ import (
 
 // using for Validators contract's initialize
 var (
-	InitValAddress = common.HexToAddress("0x513A32afC0De7eE89CE8Dc8320B1F4535b539387")
+	InitValAddress = common.HexToAddress("0x1c0e8eaf42ec8d4010e960313248d2af95be7d34")
 	InitRate       = uint8(70)
 	InitDeposit    = new(big.Int).Mul(big.NewInt(1e18), big.NewInt(1))
 )
